44afefc9b9bf3e9f1cfce786bb8042942a57ce41
Q2Computing <Q2Computing@users.noreply.github.com>
2024-04-28 17:37:55 +0000

    deploy: bf0f89641407a67b3e5f770c0d5c03f2d34ff88b

---
+++ build/q-cvMl79vT.js
@@ -0,0 +1,7 @@
+import{V as t,y as o,a as e}from"./q-tZUBM32s.js";const n=t(o(()=>e(()=>import("./q-CY_gWZLu.js"),__vite__mapDeps([])),"s_xYL1qOwPyDI")),s={title:"Q2-Computing",meta:[{name:"description",content:"Q2-Computing offers comprehensive, on-demand computational consulting support services to our clients."}]};export{n as default,s as head};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
